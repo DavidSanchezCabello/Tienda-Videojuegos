20cb3c05075c496181d1083b614a1dd888cf41fb
luisaoprog <luiszamora1995@gmail.com>
2018-02-21 12:33:28 +0100

    Testeando interfaz. Faltan JTables.

--- Tienda Videojuegos/src/main/java/Main.java
+++ Tienda Videojuegos/src/main/java/Main.java
@@ -1,11 +1,13 @@
 package main.java;
 
+import main.java.gui.Window;
+
 public class Main
 {
 	
 	public static void main(String[] args)
 	{
-		
+		new Window();
 	}
 	
 }
--- Tienda Videojuegos/src/main/java/gui/Window.java
+++ Tienda Videojuegos/src/main/java/gui/Window.java
@@ -1,6 +1,79 @@
 package main.java.gui;
 
-public class Window
-{
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+
+public class Window extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	JTabbedPane pestañas = new JTabbedPane();
 	
+	JPanel pnVid = new JPanel();
+	JPanel pnCli = new JPanel();
+	JPanel pnVent = new JPanel();
+	JButton btnNuevo = new JButton("NUEVO");
+	JButton btnBorrar = new JButton("BORRAR");
+	JButton btnModi = new JButton("MODIFICAR");
+	JButton btnVer = new JButton("VER");
+	
+	JTable tabla1 = new JTable();
+	JScrollPane scroll = new JScrollPane(tabla1);
+	
+	public Window() {
+		setLayout(null);
+		setTitle("Menú");
+		//add(scroll);
+		//pack();
+		pestañas.setBounds(60, 0, 500, 500);
+		pestañas.addTab("Videojuegos", pnVid);
+		pestañas.addTab("Clientes", pnCli);
+		pestañas.addTab("Ventas", pnVent);
+		add(pestañas);
+		
+		//pnVid.add(scroll);
+		
+		btnNuevo.setBounds(5, 20, 50, 50);
+		//icono al botón
+		ImageIcon img = new ImageIcon("nuevo.png");
+		btnNuevo.setIcon(img);
+		
+		btnBorrar.setBounds(5, 80, 50, 50);
+		ImageIcon img2 = new ImageIcon("borrar.png");
+		btnBorrar.setIcon(img2);
+		
+		btnVer.setBounds(5, 140, 50, 50);
+		ImageIcon img3 = new ImageIcon("ver.png");
+		btnVer.setIcon(img3);
+		
+		btnModi.setBounds(5, 200, 50, 50);
+		ImageIcon img4 = new ImageIcon("modi.png");
+		btnModi.setIcon(img4);
+		
+		add(btnNuevo);
+		add(btnBorrar);
+		add(btnVer);
+		add(btnModi);
+
+		
+		
+		scroll.setPreferredSize(new Dimension(400, 150));
+		//add(scroll, BorderLayout.CENTER);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(450, 300);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		//setResizable(false);
+	}
+
+	
+
 }
