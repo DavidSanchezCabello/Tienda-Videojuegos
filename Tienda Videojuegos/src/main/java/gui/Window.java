2c47a40bc8c8aaf26f856b25d6cf359afabe0535
luisaoprog <luiszamora1995@gmail.com>
2018-02-21 13:44:24 +0100

    Iconos botones.

--- Tienda Videojuegos/src/main/java/gui/Window.java
+++ Tienda Videojuegos/src/main/java/gui/Window.java
@@ -1,8 +1,6 @@
 package main.java.gui;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
 public class Window extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -20,17 +20,24 @@ public class Window extends JFrame {
 	JPanel pnVid = new JPanel();
 	JPanel pnCli = new JPanel();
 	JPanel pnVent = new JPanel();
-	JButton btnNuevo = new JButton("NUEVO");
-	JButton btnBorrar = new JButton("BORRAR");
-	JButton btnModi = new JButton("MODIFICAR");
-	JButton btnVer = new JButton("VER");
+	JButton btnNuevo = new JButton("");
+	JButton btnBorrar = new JButton("");
+	JButton btnVer = new JButton("");
+	JButton btnModi = new JButton("");
 	
-	JTable tabla1 = new JTable();
-	JScrollPane scroll = new JScrollPane(tabla1);
+	
+	
+	TableModel dataModel = new AbstractTableModel() {
+        public int getColumnCount() { return 10; }
+        public int getRowCount() { return 10;}        public Object getValueAt(int row, int col) { return new Integer(row*col); }
+    };
+    JTable table = new JTable(dataModel);
+    JScrollPane scrollpane = new JScrollPane(table);
 	
 	public Window() {
 		setLayout(null);
 		setTitle("Menú");
+		
 		//add(scroll);
 		//pack();
 		pestañas.setBounds(60, 0, 500, 500);
@@ -38,6 +45,10 @@ public class Window extends JFrame {
 		pestañas.addTab("Clientes", pnCli);
 		pestañas.addTab("Ventas", pnVent);
 		add(pestañas);
+		
+		
+		
+		pnVid.add(table);
 		
 		//pnVid.add(scroll);
 		
@@ -65,10 +76,11 @@ public class Window extends JFrame {
 
 		
 		
-		scroll.setPreferredSize(new Dimension(400, 150));
+	
 		//add(scroll, BorderLayout.CENTER);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(450, 300);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		//setResizable(false);
